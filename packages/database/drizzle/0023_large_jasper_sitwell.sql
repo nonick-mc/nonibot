90c689f375a9333f23409e24660a7993a3ceff22
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2026-08-06 13:52:34 +0900

    fixup! feat(db): 入室メッセージのダイレクトメッセージを送信する設定を追加

--- packages/database/drizzle/0023_large_jasper_sitwell.sql
+++ packages/database/drizzle/0023_large_jasper_sitwell.sql
@@ -1,5 +1,20 @@
 ALTER TABLE "public_setting"."join_message" ADD COLUMN "dm_enabled" boolean;--> statement-breakpoint
 ALTER TABLE "public_setting"."join_message" ADD COLUMN "dm_message_components" jsonb[];--> statement-breakpoint
-UPDATE "public_setting"."join_message" SET "dm_enabled" = true, "dm_message_components" = ARRAY[]::jsonb[];--> statement-breakpoint
+UPDATE "public_setting"."join_message" SET "dm_enabled" = false, "dm_message_components" = ARRAY[
+	jsonb_build_object(
+		'type', 17,
+		'components', jsonb_build_array(
+			jsonb_build_object(
+				'type', 10,
+				'content', '### WELCOME'
+			),
+			jsonb_build_object(
+				'type', 10,
+				'content', '{{user}} **({{userName}})** さん、**{{serverName}}**へようこそ！'
+			)
+		),
+		'accent_color', 5763719
+	)
+]::jsonb[];--> statement-breakpoint
 ALTER TABLE "public_setting"."join_message" ALTER COLUMN "dm_enabled" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "public_setting"."join_message" ALTER COLUMN "dm_message_components" SET NOT NULL;
