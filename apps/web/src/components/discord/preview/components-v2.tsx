ea84d0753da67640ba1543b078d0c6cd3eb0f2d0
NoNICK <87268971+nonick-mc@users.noreply.github.com>
2026-08-08 06:25:47 +0900

    style(web): プレビュー用のContainerコンポーネントのCSSを修正
    ・左方向のPaddingがアクセントカラーの有無によって変化しないように変更
    ・Paddingの値を12から16に変更

--- apps/web/src/components/discord/preview/components-v2.tsx
+++ apps/web/src/components/discord/preview/components-v2.tsx
@@ -130,7 +130,7 @@ export function Container({ component }: ComponentProps<ComponentType.Container>
             }}
           />
         )}
-        <div className={cn('flex flex-col gap-2 p-3', hasAccent && 'pl-4')}>
+        <div className='flex flex-col gap-2 p-4'>
           {component.components.map((child, i) => (
             // biome-ignore lint/suspicious/noArrayIndexKey: There are no usable elements other than the index
             <ComponentV2 key={i} component={child as PreviewComponent} />
